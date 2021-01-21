2805d468fdb1414d6617b03cc7fcd56e70e8bcb0
Brett Walton <bmw0045@auburn.edu>
2021-01-21 16:40:54 -0500

    added League Table

--- FinalProject/View/leagueTableViewCell.swift
+++ FinalProject/View/leagueTableViewCell.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class leagueTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var leagueName: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
