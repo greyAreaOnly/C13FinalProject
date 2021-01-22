85c7d98d06b8f33f32081847af82eb0adaaa925a
Brett Walton <bmw0045@auburn.edu>
2021-01-22 15:45:08 -0500

    league table updates & scheduled tab

--- FinalProject/Controller/LeagueViewController.swift
+++ FinalProject/Controller/LeagueViewController.swift
@@ -11,19 +11,23 @@ import SwiftyJSON
 
 class LeagueViewController: UIViewController {
     
+    @IBOutlet weak var tableTitle: UILabel!
     @IBOutlet weak var leagueTableView: UITableView!
     // var matches = [Match]()
     var leagues = ["Premier League", "La Liga", "Serie A", "Bundesliga", "Ligue 1", "Eredivisie"]
+    var todaysDate = Date()
+    //let formatter = DateFormatter()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableTitle.text = "Leagues"
         leagueTableView.dataSource = self
         leagueTableView.delegate = self
         leagueTableView.register(UINib(nibName: "leagueTableViewCell", bundle: nil), forCellReuseIdentifier: "leagueCell")
     }
     
     func viewWillAppear() {
-    
     }
 }
 
@@ -33,6 +37,7 @@ extension LeagueViewController: UITableViewDataSource {
         let cell = leagueTableView.dequeueReusableCell(withIdentifier: "leagueCell", for: indexPath) as! leagueTableViewCell
         let league = self.leagues[indexPath.row]
         cell.leagueName.text = "\(league)"
+        cell.leagueImage.image = UIImage(named: "\(indexPath.row)")
         return cell
     }
     
@@ -45,36 +50,50 @@ extension LeagueViewController: UITableViewDataSource {
 extension LeagueViewController: UITableViewDelegate {
   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     let storyboard = UIStoryboard(name: "Main", bundle: nil)
-//    var initialValue: String
-    
-    
+    let todaysFormattedDate = todaysDate.getFormattedDate(format: "yyyy-MM-dd")
+    let status = "FINISHED"
     switch indexPath.row {
     case 0:
         let fixtureVC = storyboard.instantiateViewController(identifier: "FixtureViewController") as! FixtureViewController
-        fixtureVC.urlFromTable = "https://api.football-data.org/v2/competitions/PL/matches?status=FINISHED&dateFrom=2021-01-10&dateTo=2021-01-25"
+        fixtureVC.urlFromTable = "https://api.football-data.org/v2/competitions/PL/matches?status=\(status)&dateFrom=2021-01-10&dateTo=\(todaysFormattedDate)"
+        fixtureVC.league = "PL"
         self.navigationController?.pushViewController(fixtureVC, animated: true)
+    
     case 1:
         let fixtureVC = storyboard.instantiateViewController(identifier: "FixtureViewController") as! FixtureViewController
-        fixtureVC.urlFromTable = "https://api.football-data.org/v2/competitions/PD/matches?status=FINISHED&dateFrom=2021-01-10&dateTo=2021-01-25"
+        fixtureVC.urlFromTable = "https://api.football-data.org/v2/competitions/PD/matches?status=\(status)&dateFrom=2021-01-10&dateTo=\(todaysFormattedDate)"
+        fixtureVC.league = "PD"
         self.navigationController?.pushViewController(fixtureVC, animated: true)
     case 2:
         let fixtureVC = storyboard.instantiateViewController(identifier: "FixtureViewController") as! FixtureViewController
-        fixtureVC.urlFromTable = "https://api.football-data.org/v2/competitions/SA/matches?status=FINISHED&dateFrom=2021-01-10&dateTo=2021-01-25"
+        fixtureVC.urlFromTable = "https://api.football-data.org/v2/competitions/SA/matches?status=\(status)&dateFrom=2021-01-10&dateTo=\(todaysFormattedDate)"
+        fixtureVC.league = "SA"
         self.navigationController?.pushViewController(fixtureVC, animated: true)
     case 3:
         let fixtureVC = storyboard.instantiateViewController(identifier: "FixtureViewController") as! FixtureViewController
-        fixtureVC.urlFromTable = "https://api.football-data.org/v2/competitions/BL1/matches?status=FINISHED&dateFrom=2021-01-10&dateTo=2021-01-25"
+        fixtureVC.urlFromTable = "https://api.football-data.org/v2/competitions/BL1/matches?status=\(status)&dateFrom=2021-01-10&dateTo=\(todaysFormattedDate)"
+        fixtureVC.league = "BL1"
         self.navigationController?.pushViewController(fixtureVC, animated: true)
     case 4:
         let fixtureVC = storyboard.instantiateViewController(identifier: "FixtureViewController") as! FixtureViewController
-        fixtureVC.urlFromTable = "https://api.football-data.org/v2/competitions/FL1/matches?status=FINISHED&dateFrom=2021-01-10&dateTo=2021-01-25"
+        fixtureVC.urlFromTable = "https://api.football-data.org/v2/competitions/FL1/matches?status=\(status)&dateFrom=2021-01-10&dateTo=\(todaysFormattedDate)"
+        fixtureVC.league = "FL1"
         self.navigationController?.pushViewController(fixtureVC, animated: true)
  
     default:
         let fixtureVC = storyboard.instantiateViewController(identifier: "FixtureViewController") as! FixtureViewController
-        fixtureVC.urlFromTable = "https://api.football-data.org/v2/competitions/DED/matches?status=FINISHED&dateFrom=2021-01-10&dateTo=2021-01-25"
+        fixtureVC.urlFromTable = "https://api.football-data.org/v2/competitions/DED/matches?status=\(status)&dateFrom=2021-01-10&dateTo=\(todaysFormattedDate)"
+        fixtureVC.league = "DED"
         self.navigationController?.pushViewController(fixtureVC, animated: true)
     }
   }
+}
+
+extension Date {
+   func getFormattedDate(format: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        return dateFormatter.string(from: self)
+    }
 }
 
